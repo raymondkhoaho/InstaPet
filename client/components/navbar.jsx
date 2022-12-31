aff23df46165f890b4f508fd274f84e57b654119
Raymond Ho <raymondkhoaho@gmail.com>
2022-12-31 12:26:58 +0000

    added icons to navbar

--- client/components/navbar.jsx
+++ client/components/navbar.jsx
@@ -3,11 +3,12 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import { Container, Nav, Navbar, Image } from 'react-bootstrap';
 
-export default function HelloWorld(props) {
+export default function NavBar(props) {
   const user = (
     <Image
       src="/images/ryleyleho1.JPG"
       alt="UserName profile image"
+      className="profile"
       roundedCircle
       style={{
         width: '50px',
@@ -23,9 +24,9 @@ export default function HelloWorld(props) {
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto d-lg-flex align-items-center">
-              <Nav.Link href="#explore">Explore</Nav.Link>
-              <Nav.Link href="#users">Users</Nav.Link>
-              <Nav.Link href="#newpost">New Post</Nav.Link>
+              <Nav.Link href="#explore"><i className="fa-solid fa-magnifying-glass" />Explore</Nav.Link>
+              <Nav.Link href="#users"><i className="icon fa-solid fa-users" />Users</Nav.Link>
+              <Nav.Link href="#newpost"><i className="fa-solid fa-circle-plus" />New Post</Nav.Link>
               <Nav.Link href="#profile">{ user }</Nav.Link>
             </Nav>
           </Navbar.Collapse>
--- client/pages/home.jsx
+++ client/pages/home.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import HelloWorld from '../components/hello-world';
+import NavBar from '../components/navbar';
 
 export default function Home(props) {
   return (
     <div>
-      <HelloWorld />
+      <NavBar />
     </div>
   );
 }
