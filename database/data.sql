1d6ec57cbb57825b190577aa63bb6139bb746745
Raymond Ho <raymondkhoaho@gmail.com>
2023-02-13 02:56:05 +0000

    added demo user to data.sql

--- database/data.sql
+++ database/data.sql
@@ -3,6 +3,7 @@ values ('ryleyleho', '$argon2i$v=19$m=4096,t=3,p=1$eEoaFr/3PFKGzuSUC22fDQ$Jwdx6s
        ('teddibendel', '$argon2i$v=19$m=4096,t=3,p=1$Qniyof8ixCDsnK3EHDIoEQ$KKDKVrff9o6/wK/lTlCA41kE6yZW1ZzAlNICLggzgwY', '/images/teddibendel1.JPG'),
        ('limbani', '$argon2i$v=19$m=4096,t=3,p=1$5maGmo4/UigU1oHGudnWGA$/fcS3YdTs3bQYLoAThSsjlRhdNkHQ+skisyTT+V8Upg', '/images/limbani1.JPG'),
        ('juniper', '$argon2i$v=19$m=4096,t=3,p=1$mRKSBlxxTqH5ccgV7jE82A$oHKgd3RIAcIzC5vA4xIgC/mfd++1XJjgREneiN0wTec', '/images/juniper1.JPG');
+       ('demo', '$argon2i$v=19$m=4096,t=3,p=1$6phTlNVkhJkepUQDvQ7dQw$qg/NSmjk1R10DkFiSUbIvImfaL8kpbuIy/uMr8s/kNA');
 
 insert into "photos" ("userId", "imageUrl", "caption")
 values (4, '/images/juniper5.JPG', 'juniper-5'),
