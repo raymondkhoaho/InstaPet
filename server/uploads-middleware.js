1852c65e0adbfdf7478f021e60bae887d35d3bc8
Raymond Ho <raymondkhoaho@gmail.com>
2023-02-16 01:01:02 +0000

    updated uploads-middleware.js for S3 AWS uploads

--- server/uploads-middleware.js
+++ server/uploads-middleware.js
@@ -1,19 +1,29 @@
 const path = require('path');
 const multer = require('multer');
+const multerS3 = require('multer-s3');
+const { S3 } = require('@aws-sdk/client-s3');
 
-const imagesDirectory = path.join(__dirname, 'public/images');
-
-const storage = multer.diskStorage({
-  destination: (req, file, callback) => {
-    callback(null, imagesDirectory);
-  },
-  filename: (req, file, callback) => {
-    const fileExtension = path.extname(file.originalname);
-    const name = `${file.fieldname}-${Date.now()}${fileExtension}`;
-    callback(null, name);
+const s3 = new S3({
+  region: process.env.AWS_S3_REGION,
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
   }
 });
 
-const uploadsMiddleware = multer({ storage }).single('image');
+const storage = multerS3({
+  s3,
+  acl: 'public-read',
+  bucket: process.env.AWS_S3_BUCKET,
+  contentType: multerS3.AUTO_CONTENT_TYPE,
+  key: (req, file, done) => {
+    const fileExtension = path.extname(file.originalname);
+    done(null, `${Date.now()}${fileExtension.toLowerCase()}`);
+  }
+});
+
+const uploadsMiddleware = multer({
+  storage
+}).single('file-to-upload');
 
 module.exports = uploadsMiddleware;
