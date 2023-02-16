469206e6fd55fd5f275ddf49ba5c111ea1f35c6a
Raymond Ho <raymondkhoaho@gmail.com>
2023-02-16 01:27:57 +0000

    modified index.js

--- server/uploads-middleware.js
+++ server/uploads-middleware.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const multer = require('multer');
 const multerS3 = require('multer-s3');
-const { S3 } = require('@aws-sdk/client-s3');
+const S3 = require('@aws-sdk/client-s3');
 
 const s3 = new S3({
   region: process.env.AWS_S3_REGION,
