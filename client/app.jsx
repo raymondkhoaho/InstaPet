236fe96abc5d6cba8b3f2e3fd16f862d8527cb64
Raymond Ho <raymondkhoaho@gmail.com>
2024-12-11 01:54:20 +0000

    refactored navbar

--- client/app.jsx
+++ client/app.jsx
@@ -94,3 +94,92 @@ export default class App extends React.Component {
 App.contextType = AppContext;
 AuthForm.contextType = AppContext;
 NavBar.contextType = AppContext;
+
+// import React, { useState, useEffect, useCallback } from 'react';
+// import jwtDecode from 'jwt-decode';
+// import AuthForm from './components/auth-form';
+// import Home from './pages/home';
+// import Users from './pages/users';
+// import UserPage from './pages/user-page';
+// import NavBar from './components/navbar';
+// import NewPost from './pages/new-post';
+// import parseRoute from './lib/parse-route';
+// import AppContext from './lib/app-context';
+// import 'react-image-lightbox/style.css';
+
+// const App = () => {
+//   const [user, setUser] = useState(null);
+//   const [token, setToken] = useState(null);
+//   const [profileImageUrl, setProfileImageUrl] = useState('');
+//   const [isAuthorizing, setIsAuthorizing] = useState(true);
+//   const [route, setRoute] = useState(parseRoute(window.location.hash));
+
+//   useEffect(() => {
+//     const handleHashChange = () => setRoute(parseRoute(window.location.hash));
+//     window.addEventListener('hashchange', handleHashChange);
+
+//     const token = window.localStorage.getItem('react-context-jwt');
+//     const user = token ? jwtDecode(token) : null;
+//     setUser(user);
+//     setIsAuthorizing(false);
+
+//     return () => window.removeEventListener('hashchange', handleHashChange);
+//   }, []);
+
+//   const handleSignIn = useCallback((result) => {
+//     const { user, token } = result;
+//     window.localStorage.setItem('react-context-jwt', token);
+//     setUser(user);
+//   }, []);
+
+//   const handleSignOut = useCallback(() => {
+//     window.localStorage.removeItem('react-context-jwt');
+//     setUser(null);
+//     window.location.hash = '';
+//   }, []);
+
+//   const renderPage = () => {
+//     const { path, params } = route;
+//     switch (path) {
+//       case '':
+//       case 'explore':
+//         return <Home />;
+//       case 'sign-in':
+//       case 'sign-up':
+//         return <AuthForm action={path} />;
+//       case 'users':
+//         return <Users />;
+//       case 'user':
+//         const username = params.get('username');
+//         return <UserPage username={username} />;
+//       case 'newpost':
+//         return <NewPost />;
+//       default:
+//         return null;
+//     }
+//   };
+
+//   const renderNavBar = () => {
+//     return user !== null ? <NavBar /> : null;
+//   };
+
+//   if (isAuthorizing) return null;
+
+//   const contextValue = {
+//     user,
+//     route,
+//     handleSignIn,
+//     handleSignOut,
+//   };
+
+//   return (
+//     <AppContext.Provider value={contextValue}>
+//       <>
+//         {renderNavBar()}
+//         {renderPage()}
+//       </>
+//     </AppContext.Provider>
+//   );
+// };
+
+// export default App;
--- client/components/navbar.jsx
+++ client/components/navbar.jsx
@@ -1,51 +1,50 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/css/bootstrap.css';
 import { Container, Nav, Navbar, Image, NavDropdown } from 'react-bootstrap';
 import AppContext from '../lib/app-context';
 
-export default class NavBar extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = null;
-  }
+const NavBar = () => {
+  const { user, handleSignOut } = useContext(AppContext);
+  const { profileImageUrl, username } = user;
 
-  render() {
-    const { profileImageUrl, username } = this.context.user;
-    const user = (
-      <Image
-        src={profileImageUrl}
-        alt={username}
-        className="user-profile-image"
-        roundedCircle
-        style={{
-          width: '50px',
-          height: '50px'
-        }}
-      />
-    );
-    return (
-      <div>
-        <Navbar collapseOnSelect className="navbar-top" fixed="top" bg="light" expand="lg">
-          <Container>
-            <Navbar.Brand className="logo fs-2" href="#explore">InstaPet</Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-              <Nav className="ms-auto d-lg-flex align-items-center">
-                <Nav.Link href="#explore"><i className="fa-solid fa-magnifying-glass p-2" />Explore</Nav.Link>
-                <Nav.Link href="#users"><i className="icon fa-solid fa-users p-2" />Users</Nav.Link>
-                <Nav.Link href="#newpost"><i className="fa-solid fa-circle-plus p-2" />New Post</Nav.Link>
-                <NavDropdown title={ user } id="basic-nav-dropdown">
-                  <NavDropdown.Item href="#profile">Profile</NavDropdown.Item>
-                  <NavDropdown.Item onClick={this.context.handleSignOut} href="">Sign out</NavDropdown.Item>
-                </NavDropdown>
-              </Nav>
-            </Navbar.Collapse>
-          </Container>
-        </Navbar>
-      </div>
-    );
-  }
-}
+  return (
+    <Navbar collapseOnSelect className="navbar-top" fixed="top" bg="light" expand="lg">
+      <Container>
+        <Navbar.Brand className="logo fs-2" href="#explore">InstaPet</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto d-lg-flex align-items-center">
+            <Nav.Link href="#explore">
+              <i className="fa-solid fa-magnifying-glass p-2" />Explore
+            </Nav.Link>
+            <Nav.Link href="#users">
+              <i className="icon fa-solid fa-users p-2" />Users
+            </Nav.Link>
+            <Nav.Link href="#newpost">
+              <i className="fa-solid fa-circle-plus p-2" />New Post
+            </Nav.Link>
+            <NavDropdown
+              title={
+                <Image
+                  src={profileImageUrl}
+                  alt={username}
+                  className="user-profile-image"
+                  roundedCircle
+                  style={{ width: '50px', height: '50px' }}
+                />
+              }
+              id="basic-nav-dropdown"
+            >
+              <NavDropdown.Item href="#profile">Profile</NavDropdown.Item>
+              <NavDropdown.Item onClick={handleSignOut} href="#">
+                Sign out
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
 
-NavBar.contextType = AppContext;
+export default NavBar;
