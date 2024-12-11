f9f88fab5d9f31e8e1cbb13848fd74710c746ece
Raymond Ho <raymondkhoaho@gmail.com>
2024-12-11 02:26:47 +0000

    refactor users

--- client/pages/users.jsx
+++ client/pages/users.jsx
@@ -1,63 +1,45 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Container, Image, Nav } from 'react-bootstrap';
 
-export default class Users extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      users: null
-    };
-  }
+const Users = () => {
+  const [users, setUsers] = useState(null);
 
-  componentDidMount() {
+  useEffect(() => {
     fetch('/api/users')
       .then(res => res.json())
-      .then(users => this.setState({ users }))
+      .then(data => setUsers(data))
       .catch(err => console.error(err));
-  }
+  }, []);
 
-  render() {
-    if (!this.state.users) return null;
-    const usersList = this.state.users;
-    const users = usersList.map(user => {
-      const { profileImageUrl, userId, username } = user;
-      return (
-        <div key={userId} className="p-1 col-12 col-md-6 col-lg-4">
-          <Nav.Link href={`#user?username=${username}`} className="card-container">
-            <div>
-              <Image
-                key={userId}
-                src={profileImageUrl}
-                alt={username}
-                className="user-profile-image p-2"
-                roundedCircle
-                style={{
-                  width: '100px',
-                  height: '100px'
-                }}
-              />
-              <p>
-                {username}
-              </p>
+  if (!users) return null;
+
+  return (
+    <Container>
+      <div>
+        <h3 className="mt-2">Users</h3>
+      </div>
+      <div>
+        <div className="user-gallery d-flex flex-wrap">
+          {users.map(({ profileImageUrl, userId, username }) => (
+            <div key={userId} className="p-1 col-12 col-md-6 col-lg-4">
+              <Nav.Link href={`#user?username=${username}`} className="card-container">
+                <div>
+                  <Image
+                    src={profileImageUrl}
+                    alt={username}
+                    className="user-profile-image p-2"
+                    roundedCircle
+                    style={{ width: '100px', height: '100px' }}
+                  />
+                  <p>{username}</p>
+                </div>
+              </Nav.Link>
             </div>
-          </Nav.Link>
+          ))}
         </div>
+      </div>
+    </Container>
+  );
+};
 
-      );
-    });
-    return (
-      <Container>
-        <div>
-          <h3 className="mt-2">
-            Users
-          </h3>
-        </div>
-        <div>
-          <div className="user-gallery d-flex flex-wrap">
-            {users}
-          </div>
-        </div>
-      </Container>
-    );
-  }
-}
+export default Users;
